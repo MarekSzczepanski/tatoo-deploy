80f95757f4297ef420c7276e40a79b68af5d48ae
Marek <marek.szczepanski1@onet.pl>
2020-03-25 16:49:34 +0100

    h

--- routes/api/admin_dashboard.route.js
+++ routes/api/admin_dashboard.route.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
+let auth = false;
 
 router.route("/auth").post((req, res) => {
-  let auth = false;
   console.log("post", req.body);
   auth = req.body;
-  router.route("/auth").get((req, res) => {
-    console.log("get", auth);
-    res.json(auth);
-  });
+});
+router.route("/auth").get((req, res) => {
+  console.log("get", auth);
+  res.json(auth);
 });
 
 module.exports = router;
