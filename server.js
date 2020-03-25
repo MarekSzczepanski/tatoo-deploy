301590dd9044be8b3fc422472dc9d02f253f8084
Marek <marek.szczepanski1@onet.pl>
2020-03-25 20:08:50 +0100

    3

--- server.js
+++ server.js
@@ -5,7 +5,30 @@ const config = require("config");
 const fileUpload = require("express-fileupload");
 const app = express();
 
-app.use(cors());
+/* app.use(cors()); */
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json());
 app.use(fileUpload());
 
@@ -56,7 +79,7 @@ if (process.env.NODE_ENV === "production") {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
-app.use(function(req, res, next) {
+/* app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader(
     "Access-Control-Allow-Headers",
@@ -67,7 +90,7 @@ app.use(function(req, res, next) {
     "POST, GET, PATCH, DELETE, OPTIONS"
   );
   next();
-});
+}); */
 /* const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`)); */
