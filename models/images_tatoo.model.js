2a709e394ff74f1e6c740046c5deb8a6d9e84efd
Marek <marek.szczepanski1@onet.pl>
2020-03-27 01:13:14 +0100

    1

--- models/images_tatoo.model.js
+++ models/images_tatoo.model.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const newsSchema = new Schema({
+const imagesSchema = new Schema({
   src: String
 });
 
-const Images_tatoo = mongoose.model("Images_tatoo", newsSchema);
+const Images_tatoo = mongoose.model("Images_tatoo", imagesSchema);
 
 module.exports = Images_tatoo;
