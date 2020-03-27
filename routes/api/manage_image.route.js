7fb41a2c0cc7ec074ff04c158a666ae0aedb6e04
Marek <marek.szczepanski1@onet.pl>
2020-03-27 03:15:31 +0100

    2

--- routes/api/manage_image.route.js
+++ routes/api/manage_image.route.js
@@ -4,6 +4,7 @@ const Images_tatoo = require("../../models/images_tatoo.model");
 
 router.route("/add_image").post((req, res) => {
   const imageSrc = req.body;
+  console.log("4444444444444444444", imageSrc);
   const newImage = new Images_tatoo({
     src: imageSrc
   });
