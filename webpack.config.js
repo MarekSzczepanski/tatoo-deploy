e7bccad62d8bbf932fa118b5c5c88d848c5cb5bf
Marek <marek.szczepanski1@onet.pl>
2020-04-11 10:35:46 +0200

    webpack test

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-const webpack = require("webpack");
+/* const webpack = require("webpack");
 const path = require("path");
 
 module.exports = {
@@ -21,3 +21,4 @@ module.exports = {
     }),
   ],
 };
+ */
